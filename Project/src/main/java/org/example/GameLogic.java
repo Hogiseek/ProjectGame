903e8380eb8ff14db808ccb8166708b193858305
Hogiseek <168132384+Hogiseek@users.noreply.github.com>
2024-05-16 09:25:49 +0200

    Add files via upload

--- Project/src/main/java/org/example/GameLogic.java
+++ Project/src/main/java/org/example/GameLogic.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class GameLogic implements KeyListener {
 
-    ArrayList<Island> islands;
+    ArrayList<Island> islands; // vytvoření několika islandů
     Island island, island1, island2, island3, island4;
     Player player;
     BackGround backGround;
@@ -22,15 +22,21 @@ public class GameLogic implements KeyListener {
         islands= new ArrayList<>();
         backGrounds = new ArrayList<>();
         player = new Player();
-        backGround = new BackGround("BackgroundGame.png",0);
+        backGround = new BackGround("BackGround2.png",0);
         backGrounds.add(backGround);
         island= new Island("Island.png", 500, 420);
-        island1= new Island("Island.png", 50, 100);
+        island1= new Island("Island.png", 400, 510);
+        island2= new Island("Island.png", 720, 640);
+        island3= new Island("Island.png", 500, 50);
+        island4= new Island("Island.png", 600, 580);
 
 
 
         islands.add(island);
         islands.add(island1);
+        islands.add(island2);
+        islands.add(island3);
+        islands.add(island4);
 
 
 
